c7d7ddafe19c2d0189fd48616bd71ea15c2c5466
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-06 13:34:35 +0200

    Pass a prop to style component
    Pass a prop to the component

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -16,7 +16,8 @@ class StarRating extends Component {
     for (let i = 0; i < maxRating; i++) {
       stars.push(
         <Star 
-          setRating={ () => this.handleSetRating() }
+          isSelected={this.state.rating > i}
+          setRating={ () => this.handleSetRating(i + 1) }
           key={i}
         />
       );
