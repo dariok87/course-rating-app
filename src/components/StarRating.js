8efdbbdc1cdbb59188bbc89e703fb3c0f3da00df
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-06 13:01:44 +0200

    Initialize a state

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import Star from './Star';
+
 
 class StarRating extends Component {
 
   // Initialize a 'rating' state
-
+  state = {
+    rating: 0
+  }
 
   // Write a function that returns 5 Star components
-
+  
 
   // Write an event handler that updates the rating state.
   // Pass the function to a Star component via props
